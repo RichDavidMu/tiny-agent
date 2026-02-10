d19f7f99c366aca77d248ef36173b06e758678cf
David <weigezailushang@gmail.com>
2026-02-10 21:29:23 +0800

    integrate with new agent task service proto

--- packages/web-app/src/stream/node/contentNodes/textNode/textNode.ts
+++ packages/web-app/src/stream/node/contentNodes/textNode/textNode.ts
@@ -1,9 +1,14 @@
 import type { TextContentBlock } from 'agent-core';
+import { makeAutoObservable } from 'mobx';
 import { BaseContentNode } from '@/stream/node/contentNodes/base.ts';
 
 export class TextNode extends BaseContentNode {
   type = 'text' as const;
   text = '';
+  constructor() {
+    super();
+    makeAutoObservable(this);
+  }
   update(chunk: TextContentBlock) {
     this.text += chunk.content_block.text;
   }
--- packages/web-app/src/stream/node/contentNodes/thinkNode/thinkNode.ts
+++ packages/web-app/src/stream/node/contentNodes/thinkNode/thinkNode.ts
@@ -1,9 +1,14 @@
 import type { TextContentBlock } from 'agent-core';
+import { makeAutoObservable } from 'mobx';
 import { BaseContentNode } from '@/stream/node/contentNodes/base.ts';
 
 export class ThinkNode extends BaseContentNode {
   type = 'thinking' as const;
   text = '';
+  constructor() {
+    super();
+    makeAutoObservable(this);
+  }
   update(chunk: TextContentBlock) {
     this.text += chunk.content_block.text;
   }
